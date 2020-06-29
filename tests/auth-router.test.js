34f6068e50fe27a31f89c4c44d54d7378de6ecfc
ashleybiermann <ashleybiermann@gmail.com>
2020-06-28 18:52:29 -0700

    updated authrouter

--- tests/auth-router.test.js
+++ tests/auth-router.test.js
@@ -2,3 +2,24 @@
 
 require('dotenv').config();
 // make a fake user, so that you can use that authentication to add a product, because the route needs authentication to add a product. 
+'use strict';
+
+/**Bearer Auth middleware Test
+ * @module bearer-auth.test
+ */
+
+require('dotenv').config();
+const supergoose = require('cf-supergoose');
+const server = require('../src/server');
+// const basicAuth = require('../src/auth/middleware/basic');
+
+const mockRequest = supergoose.server(server.server);
+
+const consoleSpy = jest.spyOn(global.console, 'log');
+
+beforeAll(() => {
+  supergoose.startDB();
+})
+afterAll(() => {
+  supergoose.stopDB();
+})
